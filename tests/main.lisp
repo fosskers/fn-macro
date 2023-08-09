044871e0ba265fb888d2bf18d52060aa6e32e6f0
Colin Woodbury <colin@fosskers.ca>
2023-08-09 18:08:37 +0900

    Several more tests

--- tests/main.lisp
+++ tests/main.lisp
@@ -4,4 +4,7 @@
 (in-package :fn-macro/tests)
 
 (define-test "Basic"
-  (is equal '(2 3 4) (mapcar (fn (+ % 1)) '(1 2 3))))
+  (is equal '(2 3 4) (mapcar (fn (+ % 1)) '(1 2 3)))
+  (is = 3 (funcall (fn (+ % % %)) 1))
+  (is = 9 (funcall (fn (+ %1 %2 %3 %4 %5 %6 %7 %8 %9))
+                   1 1 1 1 1 1 1 1 1)))
